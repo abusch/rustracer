c340cb4acbeb18e2c19584094400e2a67b06bee7
Antoine Büsch <antoine.busch@gmail.com>
2017-01-12 17:46:52 +1100

    Remove rand dependency

--- src/lib.rs
+++ src/lib.rs
@@ -9,7 +9,6 @@ extern crate image as img;
 extern crate itertools as it;
 extern crate nalgebra as na;
 extern crate num;
-extern crate rand;
 #[macro_use(o, slog_info, slog_debug, slog_warn, slog_error, slog_trace, slog_log)]
 extern crate slog;
 #[macro_use]
