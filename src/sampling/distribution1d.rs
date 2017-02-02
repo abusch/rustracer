8dbf1c7fba49b8c0d1d633af5529736759580180
Antoine Büsch <antoine.busch@gmail.com>
2017-02-02 18:55:43 +1100

    Fix some off by one bugs in Distribution1D

--- src/sampling/distribution1d.rs
+++ src/sampling/distribution1d.rs
@@ -9,22 +9,21 @@ pub struct Distribution1D {
 impl Distribution1D {
     pub fn new(f: &[f32]) -> Distribution1D {
         let n = f.len();
-        assert!(n >= 2, "Need at least 2 values in a distribution");
         let func = Vec::from(f);
         let mut cdf = vec![0.0; n + 1];
         // compute integral of step function at xi
         cdf[0] = 0.0;
-        for i in 1..n + 1 {
+        for i in 1..(n + 1) {
             cdf[i] = cdf[i - 1] + func[i - 1] / n as f32;
         }
         // transform step function integral into CDF
         let func_int = cdf[n];
         if func_int == 0.0 {
-            for i in 0..n + 1 {
+            for i in 1..(n + 1) {
                 cdf[i] = i as f32 / n as f32;
             }
         } else {
-            for i in 0..n + 1 {
+            for i in 1..(n + 1) {
                 cdf[i] /= func_int;
             }
         }
