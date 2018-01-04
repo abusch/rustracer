56f9cf4a2a4ec7650c00496f4a2bde7f4a636ffe
Antoine Büsch <antoine.busch@gmail.com>
2018-01-05 09:03:16 +1100

    Oops, forgot to commit some files...

---
+++ rustracer-core/src/material/mixmat.rs
@@ -0,0 +1,61 @@
+use std::sync::Arc;
+
+use light_arena::Allocator;
+
+use bsdf::{BxDFHolder, ScaledBxDF};
+use interaction::SurfaceInteraction;
+use material::{Material, TransportMode};
+use paramset::TextureParams;
+use spectrum::Spectrum;
+use texture::TextureSpectrum;
+
+#[derive(Debug)]
+pub struct MixMaterial {
+    mat1: Arc<Material>,
+    mat2: Arc<Material>,
+    scale: Arc<TextureSpectrum>,
+}
+
+impl MixMaterial {
+    pub fn create(mp: &TextureParams, m1: Arc<Material>, m2: Arc<Material>) -> Arc<Material> {
+        Arc::new(MixMaterial {
+            mat1: m1,
+            mat2: m2,
+            scale: mp.get_spectrum_texture("amount", &Spectrum::from(0.5)),
+        })
+    }
+}
+
+impl Material for MixMaterial {
+    fn compute_scattering_functions<'a, 'b>(
+        &self,
+        si: &mut SurfaceInteraction<'a, 'b>,
+        mode: TransportMode,
+        allow_multiple_lobes: bool,
+        arena: &'b Allocator,
+    ) {
+        let s1 = self.scale.evaluate(si).clamp();
+        let s2 = (Spectrum::white() - s1).clamp();
+        let mut si2 = si.clone();
+        self.mat1
+            .compute_scattering_functions(si, mode, allow_multiple_lobes, arena);
+        self.mat2
+            .compute_scattering_functions(&mut si2, mode, allow_multiple_lobes, arena);
+        let n1 = si.bsdf.as_ref().unwrap().bxdfs.len();
+        let n2 = si2.bsdf.as_ref().unwrap().bxdfs.len();
+
+        let mut bxdfs = BxDFHolder::new(arena);
+        for i in 0..n1 {
+            bxdfs.add(arena <- ScaledBxDF::new(si.bsdf.as_ref().unwrap().bxdfs[i], s1));
+        }
+        for i in 0..n2 {
+            bxdfs.add(arena <- ScaledBxDF::new(si2.bsdf.as_ref().unwrap().bxdfs[i], s2));
+        }
+
+        si.bsdf.as_mut().map(|b| {
+            (Arc::get_mut(b))
+                .as_mut()
+                .map(|b| b.bxdfs = bxdfs.into_slice())
+        });
+    }
+}
---
+++ rustracer-core/src/texture/mix.rs
@@ -0,0 +1,51 @@
+use std::sync::Arc;
+use std::fmt::Debug;
+use std::ops::{Add, Mul};
+
+use Transform;
+use interaction::SurfaceInteraction;
+use paramset::TextureParams;
+use spectrum::Spectrum;
+use texture::{Texture, TextureFloat};
+
+#[derive(Debug)]
+pub struct MixTexture<T> {
+    tex1: Arc<Texture<T>>,
+    tex2: Arc<Texture<T>>,
+    amount: Arc<TextureFloat>,
+}
+
+impl<T> Texture<T> for MixTexture<T>
+where
+    T: Debug,
+    T: Mul<f32, Output = T>,
+    T: Add<Output = T>,
+{
+    fn evaluate(&self, si: &SurfaceInteraction) -> T {
+        let t1 = self.tex1.evaluate(si);
+        let t2 = self.tex2.evaluate(si);
+        let amt = self.amount.evaluate(si);
+
+        t1 * (1.0 - amt) + t2 * amt
+    }
+}
+
+impl MixTexture<f32> {
+    pub fn create_float(_tex2world: &Transform, tp: &TextureParams) -> MixTexture<f32> {
+        MixTexture {
+            tex1: tp.get_float_texture("tex1", 0.0),
+            tex2: tp.get_float_texture("tex2", 1.0),
+            amount: tp.get_float_texture("amount", 0.5),
+        }
+    }
+}
+
+impl MixTexture<Spectrum> {
+    pub fn create_spectrum(_tex2world: &Transform, tp: &TextureParams) -> MixTexture<Spectrum> {
+        MixTexture {
+            tex1: tp.get_spectrum_texture("tex1", &Spectrum::black()),
+            tex2: tp.get_spectrum_texture("tex2", &Spectrum::white()),
+            amount: tp.get_float_texture("amount", 0.5),
+        }
+    }
+}
